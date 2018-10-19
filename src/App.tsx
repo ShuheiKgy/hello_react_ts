63281be6cf82a243a24d64e8c474c53d2ca9d14f
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-10-19 22:54:22 +0900

    App.tsx実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,19 @@
 import * as React from 'react';
-import CharacterList, { Character } from './CharacterList';
-import Timer from './containers/Timer';
+import { Redirect, Route, Switch } from 'react-router';
+
+import Characters from './components/Characters/CharacterList';
+import Home from './components/Home/index';
 
 import './App.css';
 
-const LIMIT = 60;
-
-interface AppState {
-  timeLeft: number;
-}
-
-class App extends React.Component<{}, AppState> {
-  timerId: NodeJS.Timer;
-
-  constructor(props: {}) {
-    super(props);
-    this.state = { timeLeft: LIMIT };
-  }
-
-  reset = () => this.setState({ timeLeft: LIMIT });
-
-  tick = () => {
-    this.setState({ timeLeft: this.state.timeLeft - 1 });
-  };
-
-  componentDidMount = () => {
-    this.timerId = setInterval(this.tick, 1000);
-  };
-
-  componentDidUpdate = () => {
-    if (this.state.timeLeft === 0) {
-      this.reset();
-    }
-  };
-
-  componentWillUnmount = () => {
-    clearInterval(this.timerId);
-  };
-
-  render() {
-    const characters: Character[] = [
-      {
-        name: '山田太郎',
-        age: 14,
-        height: 160,
-      },
-      {
-        name: '高橋あきこ',
-        age: 13,
-        height: 155,
-      },
-    ];
-
-    return (
-      <div className="container">
-        <header>
-          <h1>Newタイマー</h1>
-        </header>
-        <Timer limit={60} />
-        <CharacterList school="ABC中学校" characters={characters} />
-      </div>
-    );
-  }
-}
+const App: React.SFC<{}> = () => (
+  <div className="container">
+    <Switch>
+      <Route path="/characters/:code" component={Characters} />
+      <Route path="/" component={Home} />
+      <Redirect to="/" />
+    </Switch>
+  </div>
+);
 
 export default App;
